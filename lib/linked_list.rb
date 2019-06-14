99e412a57bad138ad3dc674a644dd9848eb9a53b
lpile <logan.pile@gmail.com>
2019-06-14 17:09:33 -0600

    Add #prepend(string) and #insert(position, string) with addition of #node_position(node, position, counter = 0) helper method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,7 +7,7 @@ class LinkedList
   end
 
   def append(string)
-    if @head == nil            # sets head
+    if @head == nil            # sets head if there's no nodes
       @head = Node.new(string)
     else                       # sets next_node to point new node
       @head.next_node = Node.new(string)
@@ -27,6 +27,39 @@ class LinkedList
     end
   end
 
+  def prepend(string)
+    if @head == nil            # sets head if there's no nodes
+      @head = Node.new(string)
+    else                       # sets new head node
+      node = Node.new(string)
+      node.next_node = @head   # sets next_node to point at old head node
+      @head = node             # resets head node
+    end
+  end
+
+  def insert(position, string)
+    if @head == nil            # sets head if there's no nodes
+      @head = Node.new(string)
+    elsif count == 1           # places new node if there's only one node in linkedlist
+      if position == 0
+        prepend(string)
+      else
+        append(string)
+      end
+    else
+      node = Node.new(string)
+      # find where to insert new node
+      left_node = node_position(@head, position - 1)
+      right_node = node_position(@head, position)
+      # insert new node between left and right
+      left_node.next_node = node
+      node.next_node = right_node
+      return node
+    end
+  end
+
+  private
+
   def node_counter(node, counter)
     return counter if node.tail?
     node_counter(node.next_node, counter += 1)
@@ -36,5 +69,10 @@ class LinkedList
     @string += node.data + " "
     return @string.strip if node.tail?
     node_string(node.next_node)
+  end
+
+  def node_position(node, position, counter = 0)
+    return node if position == counter
+    node_position(node.next_node, position, counter += 1)
   end
 end
