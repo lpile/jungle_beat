f927ca632fccac4bfc6f6d00b35e4723883d50a5
lpile <logan.pile@gmail.com>
2019-06-15 12:07:11 -0600

    Add #play

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,15 +1,20 @@
 class JungleBeat
-  attr_accessor :list
+  attr_reader :list
+
   def initialize
     @list = LinkedList.new
   end
 
   def append(data)
     string = data
-    string.split(' ').each{|i| @list.append(i)}
+    string.split(' ').each{|data| @list.append(data)}
   end
 
   def count
     @list.count
+  end
+
+  def play
+    `say -r 250 "#{@list.to_string}"`
   end
 end
