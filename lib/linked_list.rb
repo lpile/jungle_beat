6d8eed29f864a2fe30025ec667ea5bbf40010193
lpile <logan.pile@gmail.com>
2019-06-15 10:40:37 -0600

    Add comments for what each method does

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,65 +7,63 @@ class LinkedList
     @find_string = ""
   end
 
-  def empty?                     # checks if list is empty
+  def empty?                                        # checks if list is empty
     @head.nil?
   end
 
   def tail_node(node)
-    if node.tail?                # finds last node
+    if node.tail?                                   # finds last node
       return node
-    else                         # recursion to find last node
+    else                                            # recursion to find last node
       tail_node(node.next_node)
     end
   end
 
   def append(data)
     node = Node.new(data)
-    if empty?                    # sets head if empty
+    if empty?                                       # sets head if empty
       @head = node
-    else                         # adds new node to list
+    else                                            # adds new node to list
       tail_node(@head).next_node = node
     end
   end
 
   def count
-    return 0 if empty?           # returns 0 with empty
-    node_counter(@head)          # recursion for counter
+    return 0 if empty?                              # returns 0 with empty
+    node_counter(@head)                             # recursion for counter
   end
 
   def to_string
-    return "" if @head.nil?      # returns empty string when empty
-    node_string(@head)           # returns string of all nodes
+    return "" if @head.nil?                         # returns empty string when empty
+    node_string(@head)                              # returns string of all nodes
   end
 
   def prepend(data)
-    node = Node.new(data)        # sets new head node
-    node.next_node = @head       # sets next_node to point at old head node
-    @head = node                 # resets head node
+    node = Node.new(data)                           # sets new head node
+    node.next_node = @head                          # sets next_node to point at old head node
+    @head = node                                    # resets head node
   end
 
   def insert(position, data)
     node = Node.new(data)
-    # find where to insert new node
-    left_node = node_position(@head, position - 1)
+    left_node = node_position(@head, position - 1)  # find where to insert new node
     right_node = node_position(@head, position)
-    # insert new node between left and right
-    left_node.next_node = node
+    left_node.next_node = node                      # insert new node between left and right
     node.next_node = right_node
   end
 
   def find(start, amount)
-    start_node = node_position(@head, start)
-    node_find_string(start_node, amount)
+    start_node = node_position(@head, start)        # finds start of range
+    node_find_string(start_node, amount)            # returns string of all nodes in range
   end
 
   def includes?(data)
-    node_include(@head, data)
+    node_include(@head, data)                       # checks node is within list
   end
 
   def pop
-    new_tail = node_position(@head, count - 2)
-    new_tail.next_node = nil
+    new_tail = node_position(@head, count - 2)      # finds second to last node
+    new_tail.next_node = nil                        # chops off last node
   end
 
   private
